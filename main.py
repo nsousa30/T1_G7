d4485c3aabba796825adebc500ccb2eac384f3f6
nsousa30 <nsousa@ua.pt>
2023-10-23 19:34:09 +0100

    facerecognition 1.0

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from facerecognition import FaceRecognition
+
+
+def main():
+    fr = FaceRecognition()
+    fr.run_recognition()
+    
+if __name__ == "__main__":
+    
+    main()
